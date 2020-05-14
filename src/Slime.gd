9d4adca75f93ae05d162cbf53b5e1fc4e43ec997
Marvin Trappe <devlog@marvintrappe.de>
2020-05-14 22:19:20 +0200

    Demo Schaden beim Player und Healthbar Prototyp hinzugefügt.

--- src/Slime.gd
+++ src/Slime.gd
@@ -2,13 +2,13 @@ extends KinematicBody2D
 
 const MAX_HEALTH = 20
 
-onready var healthbar = $ColorRect
-onready var healthlabel = $Label
+#onready var healthbar = 
+onready var healthlabel = $HealthCounter/Label
 onready var health = MAX_HEALTH
 
 
 func _process(delta):
-	healthbar.rect_size.x = 16 * (health / MAX_HEALTH)
+	#healthbar.rect_size.x = 16 * (health / MAX_HEALTH)
 	healthlabel.text = health as String
 	if health <= 0:
 		die()
